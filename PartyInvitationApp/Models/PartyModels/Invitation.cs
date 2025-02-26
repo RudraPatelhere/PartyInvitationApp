db597c8472ba7d9cd760908eacc318cf72cbcfbd
RudraPatelhere <Rpatel1198@conestogac.on.ca>
2025-02-26 10:23:37 -0500

    fixed model code

--- PartyInvitationApp/Models/PartyModels/Invitation.cs
+++ PartyInvitationApp/Models/PartyModels/Invitation.cs
@@ -1,6 +1,38 @@
-﻿namespace PartyInvitationApp.Models.PartyModels
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartyInvitationApp.Models.PartyModels
 {
+    public enum InvitationStatus
+    {
+        InviteNotSent,
+        InviteSent,
+        RespondedYes,
+        RespondedNo
+    }
+
     public class Invitation
     {
+        [Key]
+        public int InvitationId { get; set; }  // Primary Key
+
+        [Required(ErrorMessage = "Guest Name is required")]
+        [StringLength(100)]
+        public required string GuestName { get; set; }  // FIXED: Added 'required' modifier
+
+        [Required(ErrorMessage = "Guest Email is required")]
+        [EmailAddress]
+        public required string GuestEmail { get; set; }  // FIXED: Added 'required' modifier
+
+        [Required]
+        public InvitationStatus Status { get; set; } = InvitationStatus.InviteNotSent;
+
+        // Foreign Key: Each invitation belongs to a party
+        [ForeignKey("Party")]
+        public int PartyId { get; set; }
+
+        // Navigation Property (Nullable because it's automatically set when fetching related data)
+        public Party? Party { get; set; }  // FIXED: Made nullable by adding '?'
     }
 }
--- PartyInvitationApp/Models/PartyModels/Party.cs
+++ PartyInvitationApp/Models/PartyModels/Party.cs
@@ -1,6 +1,27 @@
-﻿namespace PartyInvitationApp.Models.PartyModels
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvitationApp.Models.PartyModels
 {
     public class Party
     {
+        [Key]
+        public int PartyId { get; set; }  // Primary Key
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(255)]
+        public required string Description { get; set; }  // FIXED: Added 'required' modifier
+
+        [Required(ErrorMessage = "Event Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime EventDate { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        [StringLength(150)]
+        public required string Location { get; set; }  // FIXED: Added 'required' modifier
+
+        // Navigation property: One party can have multiple invitations
+        public List<Invitation>? Invitations { get; set; } = new List<Invitation>();  // FIXED: Made nullable
     }
 }
